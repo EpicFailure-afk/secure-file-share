7f6427d988c99a703aff4cf2e1a6a2bf08fbce04
EpicFailure-afk <as3704921@gmail.com>
2025-03-16 06:52:14 +0200

    Update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 // handle api requests
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export const registerUser = async (userData) => {
   const response = await fetch(`${API_URL}/api/auth/register`, {
